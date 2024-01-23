580a262c27a7519a3d539019c5b028ee0df0cd84
Amritaash Verma <amritaash.verma@gmail.com>
2024-01-23 17:20:58 +0530

    some

---
+++ Javascript tutorial/Chapter 4/chapter4.ts
@@ -0,0 +1,17 @@
+console.log("This is chapter 4 of the ultimate javascript course.")
+
+let new_name = "Amrita4ash Verma"//can also be executed with '' 
+console.log(new_name.length) //prints the length of the name 
+
+// `` (backtiks) can also be used to declare strings and allows us to use quotes in them
+console.log(`The name of the person following this course is ${new_name}`)
+// this will print the sentence with varible new name in it and this is called string interpolation
+
+//escape sequence characters
+
+new_name = 'Murphy\'s law' //in this we can ignore the single quote apostrophe and execute the string as normal
+console.log(new_name)
+console.log("This is sentece and, \n this will print in a new line.")
+console.log("This is a sentence and \t this will insert a tab in between.")
+console.log("This is a sentence and \r the previous words will be deleted.")
+
