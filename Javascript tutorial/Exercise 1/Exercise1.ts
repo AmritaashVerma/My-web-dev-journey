cba416479f8232fd566ea09e106c5ea2709bfeb3
Amritaash Verma <amritaash.verma@gmail.com>
2024-03-24 17:41:35 +0530

    refining of exercise 1

--- Javascript tutorial/Exercise 1/Exercise1.ts
+++ Javascript tutorial/Exercise 1/Exercise1.ts
@@ -2,11 +2,11 @@
 const prompt = require("prompt-sync")();
 const num = Math.floor((Math.random() * 100)/1)
 let guess = 0
-while (true) {
+while (guess < 11) {
 let num_guess = Number(prompt("Guess the number: "))
 if (num == num_guess) {
     console.log("You have guessed the right number!!!")
-    console.log("Your score is", 100-guess)
+    console.log("Your score is", 10-guess, "out of 10")
     break;
 }
 else if (num != num_guess)  {
@@ -14,4 +14,8 @@ else if (num != num_guess)  {
     console.log("That is how far away you are from the guess, try again!")
     guess += 1
 }
+}
+
+if (guess > 10) {
+console.log("You have lost all guesses, the correct number was", num)
 }
